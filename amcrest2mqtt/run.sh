598fa7c703a3975ed40340a5390a24a5deede843
DrJeff <DrJeff@users.noreply.github.com>
2021-02-11 01:50:08 -0800

    Update run.sh

--- amcrest2mqtt/run.sh
+++ amcrest2mqtt/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/with-contenv bashio
-CONFIG_PATH=/data/options.json
+#CONFIG_PATH=/data/options.json
 echo AMCREST_HOST="$(basshio::config 'AMCREST_HOST')"
 python  -u /app/amcrest2mqtt.py
